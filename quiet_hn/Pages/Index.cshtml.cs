e09f9647caacae4a0f41a08c03b387e3bb11280e
Greg Cooke <gtcooke94@gmail.com>
2021-03-05 16:36:36 -0500

    Initial Commit

---
+++ quiet_hn/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quiet_hn.Pages
+{
+
+    public class HackerNewsEntry
+    {
+
+        public HackerNewsEntry(string title)
+        {
+            Title = title;
+        }
+
+
+        public string Title { get; }
+    }
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<HackerNewsEntry> Entries;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Entries = new List<HackerNewsEntry>();
+        }
+
+        public void OnGet()
+        {
+            Entries.Add(new HackerNewsEntry("Test Entry"));
+        }
+    }
+}
